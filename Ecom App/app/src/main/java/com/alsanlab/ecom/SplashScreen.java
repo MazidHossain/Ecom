99b98980cededff178f53631b7b607f07fe221bf
MazidHossain <60884862+MazidHossain@users.noreply.github.com>
2020-02-10 20:05:47 +0600

    comment

--- Ecom App/app/src/main/java/com/alsanlab/ecom/SplashScreen.java
+++ Ecom App/app/src/main/java/com/alsanlab/ecom/SplashScreen.java
@@ -10,5 +10,8 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+// it is working
     }
 }
